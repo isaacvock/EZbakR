f60ced20d7095f119326d90913127adfa892ad9f
isaacvock <isaac.vock@gmail.com>
2024-02-13 21:40:03 -0500

    Started creating constructors for key data object

---
+++ R/EZbakRData.R
@@ -0,0 +1,280 @@
+#' `EZbakRDataobject` constructor for internal use
+#'
+#' \code{new_EZbakRData} efficiently creates an object of class `EZbakRData`.
+#' It does not perform any rigorous checks of the legitimacy of this object.
+#' @param cB Data frame tracking the sample ID, mutational and nucleotide content,
+#' and feature assignment of sequencing reads.
+#' @param metadf Data frame tracking features of each of the samples included
+#' in `cB`.
+new_EZbakRData <- function(cB, metadf){
+  stopifnot(is.data.frame(cB))
+  stopifnot(is.data.frame(metadf))
+  structure(list(cB = cB, metadf = metadf), class = "EZbakRData")
+}
+
+#' `EZbakRDataobject` validator
+#'
+#' \code{validate_EZbakRData} ensures that input for `EZbakRDataobject` construction
+#' is valid.
+#' @param obj An object of class `EZbakRDataobject`
+validate_EZbakRData <- function(obj){
+
+  ### Vectors of potential column names
+
+  # Mutation counts
+  mutcounts <- expand.grid(c("T", "C", "G", "A", "U", "N"),
+                           c("T", "C", "G", "A", "U", "N"))
+  mutcounts <- paste0(mutcounts[,1], mutcounts[,2])
+
+  illegal_mutcounts <- c("TT", "CC", "GG", "AA", "UU")
+
+  mutcounts <- mutcounts[!(mutcounts %in% illegal_mutcounts)]
+
+
+  ### Extract objects and info from objects
+
+  vals <- unclass(obj)
+
+  cB <- vals$cB
+  metadf <- vals$metadf
+
+  cB_cols <- colnames(cB)
+  metadf_cols <- colnames(metadf)
+
+
+  # Which mutcounts are in the cB?
+  mutcounts_in_cB <- cB_cols[cB_cols %in% mutcounts]
+
+
+  ### Does cB_cols contain sample and n?
+  if(!("sample" %in% cB_cols & "n" %in% cB_cols)){
+
+    stop(
+      "cB must include columns named sample (representing the sample ID)
+      and n (representing the number of reads with identical values for the
+      columns included in the cB)!"
+    )
+
+  }
+
+
+  ### Does cB_cols contain the necessary base count column(s)?
+  basecounts_expected <- paste0("n", substr(mutcounts_in_cB, start = 1, stop = 1))
+
+  if(!all(basecounts_expected %in% cB_cols)){
+
+    stop(
+      "Not all of the columns tracking the counts of relevant nucleotides
+      are present! For example, if you have columns called TC and GA,
+      you need to have corresponding columns called nT and nG. TC and GA
+      should represent the number of T-to-C and G-to-A mutations
+      in sequencing reads, respectively, and nT and nG should represent
+      the number of Ts and Gs in the read (if there had been no mutations in that read)."
+    )
+
+  }
+
+
+  ### Does metadf contain sample and correct tl(s)?
+
+  if(!("sample" %in% metadf_cols)){
+    stop(
+      "metadf must include a column named sample (representing the sample ID)!"
+    )
+  }
+
+  # If only one mutation type tracked in cB, then need either tl or tchase + tpulse
+  # If more than one mutation type tracked in cB, need tl_<muttype> for each
+  # <muttype (e.g., TC, GA, etc.) in the cB.
+  if(length(mutcounts_in_cB) > 1){
+
+    tl_expected <- paste0("tl_", mutcounts_in_cB)
+
+    if(!all(tl_expected %in% metadf_cols)){
+
+      stop(
+        "Not all of the relevant label times are included in the metadf.
+        For example, if your cB has columns TC and GA, your metadf must
+        include columns called tl_TC and tl_GA, representing the label times
+        for metabolic labels whose recoding yield apparent T-to-C and
+        G-to-A mutations, respectively."
+        )
+
+    }
+
+
+
+  }else{
+
+    if(!("tl" %in% metadf_cols)){
+
+      if(!all(c("tchase", "tpulse") %in% metadf_cols)){
+
+        stop(
+          "metadf must contain a column called tl (the label time if using a pulse-label design)
+          or a combination of tpulse and tchase (pulse and chase times respectively
+          if using a pulse-chase design)."
+        )
+
+      }
+
+    }
+
+  }
+
+
+  ### Check if all of the samples in cB are also in metadf
+
+  samps_cB <- unique(cB$sample)
+
+  if(!all(samps_cB %in% metadf$sample)){
+    stop("Not all samples in the cB are present in the metadf!")
+  }
+
+
+  ### Check that mutation counts can be coerced to positive integers
+
+
+  ### Check that base counts can be coerced to positive integers
+
+
+  ### Check that label times are numeric and >= 0
+
+
+
+
+}
+
+
+#' `EZbakRDataobject` helper function for users
+#'
+#' \code{EZbakRData} creates an object of class `EZbakRData` and checks the validity
+#' of the provided input.
+#' @param cB Data frame with the following columns:
+#' \itemize{
+#'  \item sample: Name given to particular sample from which data was collected.
+#'  \item mutational counts: Integers corresponding to the number of a particular
+#'  mutation seen in a sequencing read. The following column names are allowed:
+#'  \itemize{
+#'    \item TC: Number of Thymine-to-Cytosine mutations
+#'    \item TA: Number of Thymine-to-Adenine mutations
+#'    \item TG: Number of Thymine-to-Guanine mutations
+#'    \item CT: Number of Cytosine-to-Thymine mutations
+#'    \item CA: Number of Cytosine-to-Adenine mutations
+#'    \item CG: Number of Cytosine-to-Guanine mutations
+#'    \item CU: Number of Cytosine-to-Uridine mutations
+#'    \item AT: Number of Adenine-to-Thymine mutations
+#'    \item AC: Number of Adenine-to-Cytosine mutations
+#'    \item AG: Number of Adenine-to-Guanine mutations
+#'    \item AU: Number of Adenine-to-Uridine mutations
+#'    \item GT: Number of Guanine-to-Thymine mutations
+#'    \item GC: Number of Guanine-to-Cytosine mutations
+#'    \item GA: Number of Guanine-to-Adenine mutations
+#'    \item GU: Number of Guanine-to-Uridine mutations
+#'    \item TN: Number of Thymine-to-Adenine/Cytosine/Guanine mutations
+#'    \item CN: Number of Cytosine-to-Adenine/Thymine/Guanine/Uridine mutations
+#'    \item AN: Number of Adenine-to-Thymine/Cytosine/Guanine/Uridine mutations
+#'    \item GN: Number of Guanine-to-Adenine/Cytosine/Thymine/Uridine mutations
+#'    \item UN: Number of Uridine-to-Adenine/Cytosine/Guanine mutations
+#'    \item NT: Number of Adenine/Cytosine/Guanine-to-Thymine mutations
+#'    \item NC: Number of Adenine/Thymine/Guanine/Uridine-to-Cytosine mutations
+#'    \item NtoA: Number of Thymine/Cytosine/Guanine/Uridine-to-Adenine mutations. (Naming convention changed because NA taken)
+#'    \item NU: Number of Cytosine/Guanine/Adenine-to-Uridine mutations.
+#'    \item NN: Number of any kind of mutation
+#'  }
+#'  \item base nucleotide count: Integers corresponding to the number of instances of a
+#'  particular type of nucleotide whose mutations are tracked in a corresponding
+#'  mutation count column. The following column names are allowed:
+#'  \itemize{
+#'    \item nT: Number of Thymines
+#'    \item nG: Number of Guanines
+#'    \item nA: Number of Adenines
+#'    \item nC: Number of Cytosines
+#'    \item nU: Number of Uridines
+#'    \item nN: number of any kind of nucleotide
+#'  }
+#'  \item features: Any columns that cannot be interpreted as a mutation count
+#'  or base nucleotide count (and that aren't named `sample` or `n`) will be
+#'  interpreted as an ID for a genomic "feature" from which a read originated.
+#'  Common examples of features and typical column names for said features include:
+#'  \itemize{
+#'    \item Genes; common column names: gene, gene_id, gene_name, GF
+#'    \item Genes-exonic; common column names: gene_exon, gene_id_exon, gene_name_exon, XF
+#'    \item Transcripts; common column names: transcripts, TF
+#'    \item Exonic bins; common column names: exonic_bins, EF, EB
+#'    \item Exons; common column names: exons, exon_ids
+#'  }
+#'  In some cases, a read will often map to multiple features (e.g., exons). Many
+#'  functions in bakR expect each of the feature IDs in these cases to be separated
+#'  by `+`. For example, if a read overlaps with two exons, with IDs exon_1 and exon_2,
+#'  then the corresponding entry in a  column of exonic assignments would be "exon_1+exon_2".
+#'  The default expectation can be overwritten though and is thus not strictly enforced.
+#'  \item n: Number of reads with identical values for all other columns.
+#' }
+#' @param metadf Data frame detailing various aspects of each of the samples included
+#' in the cB. This includes:
+#' \itemize{
+#'  \item `sample`: The sample ID, which should correspond to a sample ID in the provided cB.
+#'  \item `tl`: Metabolic label time. There are several edge cases to be aware of:
+#'  \itemize{
+#'    \item If more than one metabolic label was used in the set of samples described
+#'    by the metadf (e.g., s4U and s6G were used), then the `tl` column should be
+#'    replaced by `tl_<muttype>`, where `<muttype>` represents the corresponding mutation
+#'    type count column in the cB that the label whose incubation time will be listed
+#'    in this column. For example, if feeding with s4U in some samples and s6G in others,
+#'    then performing standard nucleotide recoding chemistry, you will include
+#'    `tl_TC` and `tl_GA` columns corresponding to the s4U and s6G label times, respectively.
+#'    \item If a pulse-chase experimental design was used (!!this is strongly discouraged
+#'    unless you have a legitimate reason to prefer this design to a pulse-label
+#'    design!!), then you should have columns named `tpulse` and `tchase`, corresponding
+#'    to the pulse and chase times respectively. The same _<muttype> convention should
+#'    be used in the case of multi-label pulse-chase designs.
+#'  }
+#' \item sample characteristics: The remaining columns can be named whatever you like
+#' and should include distinguishing features of groups of samples. Common columns might
+#' include:
+#' \itemize{
+#'  \item `treatment`: The experimental treatment applied to a set of samples.
+#'  This could represent things like genetic knockouts or knockdowns, drug treatments, etc.
+#'  \item `batch`: An ID for sets of samples that were collected and/or processed together.
+#'  Useful for regressing out technical batch effects
+#'  }
+#' \item `assay`: This optional column should include a string that
+#' describes the type of experiment that was done so as to influence
+#' how EZbakR analyzes and interprets the data from those samples.
+#' Possible values for `assay` currently include:
+#' \itemize{
+#'  \item standard: Refers to the "standard" nucleotide recoding RNA-seq methods
+#'  (e.g., TimeLapse-seq, SLAM-seq, TUC-seq, etc.), in which cells are fed with a
+#'  single metabolic label, RNA is extracted and sequenced, and mutations of a particular
+#'  type are counted
+#'  \item STL: Refers to Start-TimeLapse-seq, a method combining Start-seq (developed
+#'  by Karen Adelman's lab) with TimeLapse-seq. Used to infer the kinetics of
+#'  transcription initiation and promoter-proximal pause site departure.
+#'  \item TT: Refers to Transient-Transcriptome NR-seq, a method combining TT-seq (developed
+#'  by Patrick Cramer's lab) with NR-seq. TT-seq involves biochemically enriching
+#'  for labeled RNA. By combining this method with nucleotide recoding chemistry (as was
+#'  first done by the Simon lab with TT-TimeLapse-seq and has since been done with SLAM
+#'  chemistry, often referred to as TTchem-seq), it is possible to bioinformatically
+#'  filter out reads coming from unlabeled RNA background.
+#'  \item TILAC: Refers to TILAC, a method developed by the Simon lab to achieve
+#'  spike-in free normalization of RNA-seq data through the use of a dual labeling
+#'  approach inspired by the proteomic method SILAC.
+#'  \item subcellular: Refers to techniques such as subcellular TimeLapse-seq (developed
+#'  by Stirling Churchman's lab) which combine subcellular fractionation with
+#'  NR-seq to infer additional kinetic parameters.
+#'  \item sc: Refers to single-cell RNA-seq implementations of NR-seq.
+#'  }
+#'
+#' }
+#' @return An EZbakRData object. This is simply a list of the provide `cB` and
+#' `metadf` with class `EZbakRData`
+#' @export
+EZbakRData <- function(cB, metadf){
+
+  cB <- setDT(cB)
+  metadf <- setDT(metadf)
+
+  validate_EZbakRData(new_EZbakRData(cB, metadf))
+
+}
--- R/simple-functions.R
+++ R/simple-functions.R
@@ -1,7 +1,7 @@
 #' Simple simulation function
 #'
 #' @param nreads Number of reads to simulate
-#' @param fn Fraction of reasd that are new in simulation. Whether a read will
+#' @param fn Fraction of reads that are new in simulation. Whether a read will
 #' be new will be determined by a draw from a Bernoulli(fn) distribution.
 #' @param pnew T-to-C mutation rate in new reads
 #' @param pold T-to-C mutation rate in old reads
@@ -13,8 +13,19 @@ SimpleSim <- function(nreads = 1000, fn = 0.5,
                       pnew = 0.05, pold = 0.001,
                       rlen = 100, Ucont = 0.25){
 
+  ### CHECK INPUT
+
+  args <- c(as.list(environment()))
+
+
+  check_SimpleSim_input(args)
+
+
+  ### SIMULATE DATA
+
   # The dumb hack that gets check() to shut up
   TC <- nT <- NULL
+
 
   # Determine newness status of each read
     # 1 = new; 0 = old
@@ -35,6 +46,70 @@ SimpleSim <- function(nreads = 1000, fn = 0.5,
     dplyr::count()
 
   return(sim_df)
+
+
+}
+
+
+check_SimpleSim_input <- function(args){
+
+
+  ### nreads
+  if(!is.numeric(args$nreads)){
+    stop("nreads must be numeric!")
+  }
+
+  if(args$nreads <= 0){
+    stop("nreads must be > 0!")
+  }
+
+
+  ### fn
+  if(!is.numeric(args$fn)){
+    stop("fn must be numeric!")
+  }
+
+  if(args$fn < 0){
+    stop("fn must be >= 0!")
+  }
+
+  if(args$fn > 1){
+    stop("fn must be <= 1!")
+  }
+
+
+  ### pnew
+  if(!is.numeric(args$pnew)){
+    stop("pnew must be numeric!")
+  }
+
+  if(args$pnew < 0){
+    stop("pnew must be >= 0!")
+  }
+
+  if(args$pnew > 1){
+    stop("pnew must be <= 1!")
+  }
+
+
+  ### pold
+  if(!is.numeric(args$pnew)){
+    stop("pnew must be numeric!")
+  }
+
+  if(args$pnew < 0){
+    stop("pold must be >= 0!")
+  }
+
+  if(args$pold > 1){
+    stop("pold must be <= 1!")
+  }
+
+
+  ### pold vs. pnew
+  if(args$pold >= args$pnew){
+    stop("pold must be strictly less than pnew!")
+  }
 
 
 }
