14ba2baef3598ce3bd837bffb3d38d43b6e13793
isaacvock <isaac.vock@gmail.com>
2024-02-22 09:16:56 -0500

    Made output more sensible and started working on simulations

--- R/Fraction_New.R
+++ R/Fraction_New.R
@@ -4,35 +4,132 @@
 #' @param features Character vector of the set of features you want to stratify
 #' reads by and estimate proportions of each RNA population. The default of "all"
 #' will use all feature columns.
-#' @param populations Character vector of the set of mutational populations
-#' that you want to infer the fractions of. For example, say your cB file contains
-#' columns tracking T-to-C and G-to-A
+#' @param mutrate_populations Character vector of the set of mutational populations
+#' that you want to infer the rates of mutations for.
+#' @param fraction_design "Design matrix" specifying which RNA populations exist
+#' in your samples
 #' @param pnew_strategy String denoting which new read mutation rate estimation strategy to use.
 #' Options include:
 #' \itemize{
 #'  \item standard: Estimate a single new read and old read mutation rate for each
 #'  sample. This is done via a binomial mixture model aggregating over
-#'  \item intronic: Estimate sample-wide pnew from intronic mutation rate. Have to
-#'  specify which feature in the EZbakRDataobject cB represents the intronic reads
-#'  \item hierarchical: Estimate feature-specific pnew with standard , regularizing the feature-specific
+#'  \item hierarchical: Estimate feature-specific mutation rate with standard , regularizing the feature-specific
 #'  estimate with a sample-wide prior.
+#'  \item smalec: Estimate two old read mutation rates, as was done in
+#'  Smalec et al., 2023. Idea is that alignment artifacts can give rise to a
+#'  high mutation rate old read population that should be accounted for
+#'  to avoid overestimating the fraction of new reads
 #' }
-#' @import data.table
 #' @param pold_strategy String denoting which old read mutation rate estimation strategy
+#' @import data.table
+#' @importFrom magrittr %>%
 EstimateFractions <- function(obj, features = "all",
-                              populations = "all",
+                              mutrate_populations = "all",
+                              fraction_design = NULL,
                               pnew_strategy = "standard",
                               pold_strategy = "standard"){
 
-  mutation_rates <- EstimateMutRates(obj,
+  `.` <- list
+
+  ### Check that input is valid
+
+  args <- c(as.list(environment()))
+
+  # check_EstimateFractions_input(args)
+
+
+  ### Estimate mutation rates
+  obj <- EstimateMutRates(obj,
+                                     populations = mutrate_populations,
                                      pnew_strategy = pnew_strategy,
                                      pold_strategy = pold_strategy)
 
+  mutation_rates <- obj$mutation_rates
+
+  ### Figure out which features will be analyzed
+
+  # cB columns
+  cB <- obj$cB
+  cB_cols <- colnames(cB)
+
+  # Mutation columns in cB
+  mutcounts_in_cB <- find_mutcounts(obj)
+
+  # Base count columns in cB
+  basecounts_in_cB <- paste0("n", substr(mutcounts_in_cB, start = 1, stop = 1))
+
+  # feature columns
+  features_in_cB <- cB_cols[!(cB_cols %in% c(mutcounts_in_cB,
+                                           basecounts_in_cB,
+                                           "sample", "n"))]
+
+  # Need to determine which columns of the cB to group reads by
+  if(features == "all"){
+
+    features_to_analyze <- features_in_cB
+
+  }else{
+
+    if(!all(features %in% features_in_cB)){
+
+      stop("features includes columns that do not exist in your cB!")
+
+    }else{
+
+      features_to_analyze <- features
+
+    }
+
+  }
 
 
+  ### Estimate fraction new for each feature in each sample
+  cB <- setDT(cB)
 
+  # Pair down design matrix
+  fraction_design <- as_tibble(fraction_design)
+  mutrate_design <- fraction_design %>%
+    dplyr::filter(present) %>%
+    dplyr::select(-present)
+
+  # Estimate fractions
+  fns <- cB[,.(mixture_fit = list(fit_general_mixture(dataset = .SD,
+                                                      mutrate_design = mutrate_design,
+                                                      mutcols = mutcounts_in_cB,
+                                                      basecols = basecounts_in_cB,
+                                                      pnew = sapply(mutcounts_in_cB,
+                                                                    function(name) mutation_rates[[name]]$pnew),
+                                                      pold = sapply(mutcounts_in_cB,
+                                                                    function(name) mutation_rates[[name]]$pold)) )),
+            by = c("sample", features_to_analyze)]
+
+
+  # Unroll the fraction estimates
+  fns <- cbind(fns %>% dplyr::select(-mixture_fit),
+               as.data.frame(fns$mixture_fit))
+
+
+  # What should output be named?
+  fraction_vect <- paste(c("fractions", features_to_analyze), collapse = "_")
+
+  obj[[fraction_vect]] <- fns
+
+  # Add new class information
+  if(!("EZbakRFractions" %in% class(obj))){
+
+    class(obj) <- c("EZbakRFractions", class(obj))
+
+  }
+
+  return(obj)
 
 }
+
+# check_EstimateFractions_input(args){
+#
+#
+#
+# }
 
 
 
@@ -105,8 +202,8 @@ EstimateMutRates <- function(obj,
     mutest_temp[, c("p1", "p2") := .(inv_logit(sapply(params, `[[`, 2)),
                                      inv_logit(sapply(params, `[[`, 3)))]
 
-    mutest_temp[,c(paste0("pold_", muts_analyze[i]),
-                   paste0("pnew_", muts_analyze[i])) := .(min(c(p1, p2)),
+    mutest_temp[,c("pold",
+                   "pnew") := .(min(c(p1, p2)),
                                         max(c(p1, p2)))]
 
     mutest_temp[,c("p1", "p2") := .(NULL, NULL)]
@@ -115,8 +212,21 @@ EstimateMutRates <- function(obj,
 
   }
 
+  names(mutest) <- muts_analyze
 
-  return(mutest)
+
+  obj$mutation_rates <- mutest
+
+
+  # Add new class information
+  if(!("EZbakRMutrates" %in% class(obj))){
+
+    class(obj) <- c("EZbakRMutrates", class(obj))
+
+  }
+
+
+  return(obj)
 
 }
 
@@ -167,3 +277,109 @@ find_mutcounts <- function(obj){
 
 
 }
+
+# Softmax function
+softmax <- function(vect){
+
+  return(exp(vect)/sum(exp(vect)))
+
+}
+
+# Abstract the concept of an NR-seq mixture model to oblivion
+generalized_likelihood <- function(param, dataset,
+                                   mutrate_design, pnew, pold,
+                                   mutcols, basecols){
+
+
+  likelihoods <- rep(0, times = length(dataset[[1]]))
+
+  # Fractions of populations
+  fractions <- softmax(param)
+
+
+  # Get full likelihood for generalized mutational models
+  for(i in 1:nrow(mutrate_design)){
+
+    L <- 1
+
+    for(j in 1:ncol(mutrate_design)){
+
+      # data.table syntax is an act of violence
+      L <- L*dbinom(dataset[[mutcols[j]]],
+                    dataset[[basecols[j]]],
+                    prob = as.numeric(mutrate_design[i, j])*pnew[j] +
+                      pold[j]*(1 - as.numeric(mutrate_design[i, j])))
+
+
+
+    }
+
+    likelihoods <- likelihoods + L*fractions[i]
+
+  }
+
+  return(-sum(dataset[["n"]]*log(likelihoods)))
+
+}
+
+# Fit and process the output of the generalized likelihood model
+fit_general_mixture <- function(dataset, mutrate_design,
+                                pnew, pold, mutcols, basecols){
+
+
+  fit <- optim(par = rep(0, times = nrow(mutrate_design)),
+                         fn = generalized_likelihood,
+                         dataset = dataset,
+                         mutrate_design = mutrate_design,
+                         pnew = pnew,
+                         pold = pold,
+                         mutcols = mutcols,
+                         basecols = basecols)
+
+  # Figure out what to call each of the fractions
+    # Probably "p""muttype""old/new"_"muttype""old/new"
+  muttypes <- colnames(mutrate_design)
+
+  # Get vector of population statuses (new and old) for naming the output list
+  population_list <- vector(mode = "list", length = nrow(mutrate_design))
+  for(i in 1:nrow(mutrate_design)){
+
+    population_vector <- rep("", times = ncol(mutrate_design))
+    count <- 1
+    for(j in 1:ncol(mutrate_design)){
+
+      if(as.logical(mutrate_design[i, j])){
+
+        population_vector[count] <- "high"
+
+      }else{
+
+        population_vector[count] <- "low"
+
+      }
+
+      count <- count + 1
+
+
+    }
+
+    population_list[[i]] <- population_vector
+
+  }
+
+
+  # Make names for the output list that are easily interpretable (I hope)
+  listnames <- lapply(population_list, function(x) paste0(muttypes, x))
+  listnames <- sapply(listnames, function(x) paste(x, collapse = "_"),
+                      simplify = "vector")
+
+
+
+  outlist <- as.list(fit$par)
+  names(outlist) <- listnames
+
+  return(outlist)
+
+
+}
+
--- R/simple-functions.R
+++ R/simple-functions.R
@@ -113,3 +113,116 @@ check_SimpleSim_input <- function(args){
 
 
 }
+
+
+SimpleMultiPopSim <- function(nreads = 1000, populations = c("TC"),
+                              high_mutrates = c(0.05), low_mutrates = c(0.002),
+                              fractions = NULL,
+                              Nuc_cont = 0.25, rlen = 100, ngenes = 1){
+
+
+  ### label vectors with population identities
+  names(high_mutrates) <- populations
+  names(low_mutrates) <- populations
+  num_pops <- length(populations)
+
+
+  ### Number of reads for each gene
+  nreads_per_gene <- as.vector(rmultinom(1,
+                               size = nreads,
+                               prob = rep(1, times = ngenes)))
+
+
+  ### Simulate or extract proportions for each population
+  # ordered in such a way that elements 1:ngenes are fractions for population 1,
+  # elements (ngenes+1):2*ngenes are factions for population 2, etc.
+  if(is.null(fractions)){
+
+    fractions <- rep(1/(2^num_pops + 1),
+                     times = 2^num_pops)
+
+
+  }
+
+
+  ### Simulate data for each population
+
+  # Truth tables for presence of each population
+  mutlist <- vector(mode = "list", length = length(populations))
+
+  for(p in seq_along(populations)){
+
+    mutlist[[p]] <- 0:1
+
+  }
+
+  names(mutlist) <- populations
+
+  # Create mutational design matrix
+  mutdesign <- expand.grid(mutlist)
+
+
+  # Nucleotide content
+  nucleotides <- unique(substr(populations, start = 1, stop = 1))
+  nucleotide_cnts <- paste0("n", nucleotides)
+
+  # Mutation
+  datalist <- vector(mode = "list", length = (num_pops*length(nucleotides) + 2))
+
+  names(datalist) <- c(populations, nucleotide_cnts, "sample", "gene")
+
+  datalist[["sample"]] <- "Simulation"
+  datalist[["gene"]] <- rep(1:ngenes, times = nreads_per_gene)
+
+  browser()
+  # Populate nucleotide counts
+  for(n in seq_along(nucleotide_cnts))
+
+    datalist[[nucleotide_cnts[n]]] <- round(rlen*Nuc_cont)
+
+  for(p in seq_along(populations)){
+
+
+
+    if(length(fractions) > num_pops){
+
+      index_low <- (p-1)*ngenes + 1
+      index_high <- p*ngenes
+
+      newness <- rbinom(nreads,
+                        size = 1,
+                        rep(fractions[index_low:index_high],
+                            times = nreads_per_gene))
+
+    }else{
+
+      identity <- rmultinom(1, size = nreads,
+                            probs = fractions)
+
+      newnewss
+
+    }
+
+    datalist[[populations[p]]] <- rbinom(nreads,
+                            size = round(rlen*Nuc_cont),
+                            prob = newness*(high_mutrates[p]) +
+                              (1 - newness)*(low_mutrates[p]))
+
+
+
+  }
+
+
+  data_df <- dplyr::as_tibble(as.data.frame(datalist))
+
+  data_df <- data_df %>%
+    dplyr::group_by_all() %>%
+    dplyr::count()
+
+  return(data_df)
+
+
+
+
+
+}
