a3abc5a538ff3aef07a0144050a61649153cd1b7
isaacvock <isaac.vock@gmail.com>
2024-07-11 10:39:21 -0400

    Implement generalized dynamical systems modeling

--- R/ODEs.R
+++ R/ODEs.R
@@ -1,8 +1,124 @@
-EZfitode <- function(obj,
+#' Generalized dynamical systems modeling
+#'
+#' `EZDynamics()` estimates parameters of a user-specified dynamical systems
+#' model. The dynamical system model is specified through an adjacency matrix,
+#' which is an NxN matrix described below (see `graph` documentation). Modeling
+#' can either be done for species all assayed in each sample, or species that
+#' are assayed across a set of independent samples (e.g., subcellular fractionation
+#' involves assaying different species in different samples).
+#'
+#' @param obj Currently must be an EZbakRData object on which `AverageAndRegularize`
+#' has been run. In the future, will also support (in case where all species are
+#' assayed in every sample) providing output of just `EstimateFractions()` as input,
+#' acting as a generalization of `EstimateKinetics()` in that case.
+#' @param graph An NxN adjacency matrix, where N represents the number of species
+#' being modeled. One of these species must be called "0" and represent the "no RNA"
+#' species. This is the species from which some species are synthesized (e.g., 0 -> P,
+#' means premature RNA is synthesized from no RNA), and the species to which
+#' some species are degraded (e.g., M -> 0 means mature RNA is converted to "no RNA"
+#' via degradation). The rows and columns of this matrix must be the names of all
+#' modled species, and rownames(graph) == colnames(graph). Entry i,j of the matrix
+#' is either 0 if species i cannot be converted into species j under your model,
+#' and an integer from 1:npars (where npars = total number of parameters to be
+#' estimated) if it can.
+#'
+#' For example, the model 0 -> P -> M -> 0 would have the `graph`:
+#' `matrix(c(0, 1, 0, 0, 0, 2, 3, 0, 0), nrow = 3, ncol = 3, byrow = TRUE`.
+#' @param sub_features Which feature columns distinguish between the different
+#' measured species. Note, the measured species need not have the same name,
+#' and may not be directly equivalent to, the modeled species. The relationship
+#' between the modeled species in `graph` and `sub_features` needs to be specified
+#' in `modeled_to_measured` if the names are not equivalent though.
+#' @param grouping_features Which features are the overarching feature assignments
+#' by which `sub_features` should be grouped? This will usually be the feature
+#' columns corresponding to full-gene assignments, as well as any higher order
+#' assignments (e.g., chromosome). A `sub_feature` can be included in `grouping_features`
+#' if it never has the value of `unassigned_name` ("__no_feature" by default). Only
+#' one `sub_feature` should ever fulfill this criterion though.
+#' @param sample_feature If different samples involve assaying different species,
+#' this must be the name of the metadf column that distinguishes the different
+#' classes of samples. For example, if analyzing a subcellular fractionation
+#' dataset, you likely included a column in your metadf called "compartment".
+#' This would then be your `sample_feature`, assuming you ran `AverageAndRegularize()`,
+#' with a `mean_formula` that included compartment as a term.
+#' @param modeled_to_measured If `sub_features` is not identical to the non "0"
+#' species names in `graph`, then you must specify the relationship between
+#' `sub_features`, `sample_feature` (if specified), and the species in `graph`.
+#' This is done through a list of formulas, whose left side is a `sub_feature`
+#' and whose right side is a function of species in `graph`. If `sample_feature`
+#' is not specified, then `modeled_to_measured` should be a nameless list of
+#' formulae. If `sample_feature` is specified, then `modeled_to_measured` must
+#' be a named list of formulas where the names correspond to unique values of
+#' `sample_feature`. In this latter case, the elements, should be the mapping
+#' of measured features (`sub_features`) to modeled species (names in `graph`
+#' that aren't "0").
+#'
+#' For example, if your model is 0 -> P -> M -> 0, where P stands for premature RNA
+#' and M stands for mature RNA, and you have a column called
+#' GF that corresponds to assignment anywhere in the gene, and XF that corresponds
+#' to assignment of exclusively exon mapping reads, then your `modeled_to_measured`
+#' should be `list(GF ~ P + M, XF ~ M).`.
+#'
+#' As another example, if your model is 0 -> N -> C -> 0, where N stands for
+#' "nuclear RNA" and C stands for "cytoplasmic RNA", and your `sample_feature`
+#' takes on values of "nuclear", "cytoplasmic", and "total", and you have a single
+#' `sub_feature` called XF, then your `modeled_to_measured` shouild be
+#' `list(nuclear = GF ~ N, cytoplasmic = GF ~ C, total = GF ~ C + N)`. This is
+#' interpreted as meaning in groups of samples for which `sample_feature` == "nuclear",
+#' reads assigned to a GF are from the N species (nuclear RNA). When
+#' `sample_feature` == "cytoplasmic", reads assigned to GF correspond to the C
+#' species (cytoplasmic RNA). When `sample_feature` == "total", reads assigned
+#' to GF correspond to a combination of N and C (nuclear and cytoplasmic RNA).
+#' @param parameter_names Vector of names you would like to give to the estimated
+#' parameters. ith element should correspond to name of parameter given the ID
+#' i in `graph`. By default, this is just ki, where i is this numerical index.
+#' @param unassigned_name What value will a `sub_feature` column have if a read
+#' was not assigned to said feature? "__no_feature" by default.
+#' @param type What type of table would you like to use? Currently only supports
+#' "averages", but will support "fractions" in the near future.
+#' @param features Character vector of the set of features you want to stratify
+#' reads by and estimate proportions of each RNA population. The default of "all"
+#' will use all feature columns in the `obj`'s cB.
+#' @param populations Mutational populations that were analyzed to generate the
+#' fractions table to use. For example, this would be "TC" for a standard
+#' s4U-based nucleotide recoding experiment.
+#' @param fraction_design "Design matrix" specifying which RNA populations exist
+#' in your samples. By default, this will be created automatically and will assume
+#' that all combinations of the `mutrate_populations` you have requested to analyze are
+#' present in your data. If this is not the case for your data, then you will have
+#' to create one manually. See docs for `EstimateFractions` (run ?EstimateFractions()) for more details.
+#' @param parameter Parameter to average across replicates of a given condition.
+#' Has to be "logit_fraction_high<muttype>", where <muttype> is the type of
+#' mutation modeled in `EstimateFractions()` (e.g, TC) in this case.
+#' @param mean_vars Sample features from metadf that were used in formula for
+#' parameter average linear model.
+#' @param sd_vars Sample features from metadf that were used in formula for
+#' parameter standard deviation linear model.
+#' @param repeatID If multiple `fractions` tables exist with the same metadata,
+#' then this is the numerical index by which they are distinguished.
+#' @param exactMatch If TRUE, then `features` and `populations` have to exactly match
+#' those for a given fractions table for that table to be used. Means that you can't
+#' specify a subset of features or populations by default, since this is TRUE
+#' by default.
+#' @importFrom magrittr %>%
+#' @export
+EZDynamics <- function(obj,
                      graph,
-                     grouping_features,
                      sub_features,
-                     parameter_names = paste0("k", 1:(nrow(graph)-1))){
+                     grouping_features,
+                     sample_feature = NULL,
+                     modeled_to_measured = NULL,
+                     parameter_names = paste0("k", 1:(nrow(graph)-1)),
+                     unassigned_name = "__no_feature",
+                     type = "averages",
+                     features = NULL,
+                     populations = NULL,
+                     fraction_design = NULL,
+                     parameter = NULL,
+                     mean_vars = NULL,
+                     sd_vars = NULL,
+                     repeatID = NULL,
+                     exactMatch = TRUE){
 
   ##### ORDER OF OPERATIONS
   # 1) Infer homogeneous ODE system matrix representation (A)
@@ -19,6 +135,309 @@ EZfitode <- function(obj,
   # adjacency matrix.
   # 4)
 
+  ##### DESIRED FEATURES
+  # 1) Should be able to take averages or fractions as input. Latter only applicable
+  # if not cross-sample integration needs to be done (e.g., modeling P -> M).
+  # 2) Would love to assess dynamic range of parameter estimates using log-likelihood
+  # profile to let users know what they can say about certain parameters
+
+  ### Input
+
+  # Tidy averages table (currently makes hard assumption about interaction terms being only terms)
+  table_name <- EZget(type = type, features = features,
+                      populations = populations, fraction_design = fraction_design,
+                      parameter = parameter, mean_vars = mean_vars,
+                      sd_vars = sd_vars, repeatID = repeatID,
+                      exactMatch = exactMatch, returnNameOnly = TRUE)
+
+  if(length(table_name) > 1){
+    stop("More than one table matches your search criterion!")
+  }
+
+  table <- obj[[type]][[table_name]]
+
+  if(type == "averages"){
+
+    pivot_columns <- obj[['metadata']][['averages']][[table_name]][['mean_vars']]
+    features <- obj[['metadata']][['averages']][[table_name]][['features']]
+
+    # Currently making hard assumption of interactions only...
+    # TO-DO: Loosen this restriction
+    # Also assuming that mean_formula == sd_formula
+    pattern <- paste0("(.*)_", paste(paste0(pivot_columns, "(.+)"), collapse = ":"))
+
+    # Need to remove _posterior suffix (maybe shouldn't be there at all?)
+    # to make general pivoting strategy work
+    colnames(table) <- gsub("_posterior$", "", colnames(table))
+
+    # Tidy data!
+    tidy_avgs <- table %>%
+      pivot_longer(
+        cols = -!!features,
+        names_to = c(".value", pivot_columns),
+        names_pattern = pattern
+      )
+
+
+    # formula list
+    if(is.null(modeled_to_measured)){
+
+      species <- rownames(graph)[rownames(graph) != "0"]
+      modeled_to_measured <- vector(mode = "list", length = length(species))
+      for(s in seq_along(species)){
+        modeled_to_measured[[s]] <- as.formula(paste0(species[s], "~", species[s]))
+      }
+
+    }
+
+    if(is.null(names(modeled_to_measured))){
+
+      modeled_to_measured <- list(total = modeled_to_measured)
+      tidy_avgs <- tidy_avgs %>%
+        dplyr::mutate(imputed_sample_feature = 'total')
+
+      sample_feature <- "imputed_sample_feature"
+
+    }
+
+
+    ##### STEPS
+    # 1) Infer general solution as I do in simulation
+    # 2) Probably need to tidy up the averages table, which may be easier said then done
+    # There should be a mean, sd, and coverage column, as well as the feature columns,
+    # and finally any group detail columns (tl, compartment, etc.)
+
+
+
+    ### FIT MODEL
+
+    # Determine initial vector of parameters and their upper and lower bounds
+    npars <- max(graph)
+
+    # Have to add a bit of noise because equal parameters means general solution
+    # breaks down as their aren't N eigenvalues. Could generalize to case
+    # of equal parameters, but thinking of parameter estimates as continuous
+    # random variables that thus have probability of 0 of being equal.
+    lower_bounds <- rnorm(npars,
+                          -10,
+                          0.01)
+    upper_bounds <- rnorm(npars,
+                          10,
+                          0.01)
+    starting_values <- rnorm(npars,
+                             0, 0.01)
+
+
+    ### Infer which measured species each row of tidy_avgs belongs to
+    feature_mat <- tidy_avgs %>%
+      dplyr::select(!!sub_features) %>%
+      as.matrix()
+
+    feature_mat <- feature_mat == unassigned_name
+
+    # Are any species always assigned?
+    fm_cols <- colnames(feature_mat)
+    always_assigned <- fm_cols[which(colSums(feature_mat) == nrow(feature_mat))]
+
+    if(length(always_assigned) > 1){
+      stop("There are multiple sub_features that never have a value of `unassigned_name`!
+           There is not way for EZbakR to determine which measured species a row
+           corresponds to in this case.")
+    }
+
+    somtimes_assigned <- fm_cols[fm_cols != always_assigned]
+
+    measured_species <- apply(feature_mat, 1, function(row) {
+
+      valid_cols <- sometimes_assigned[!row[fm_cols != always_assigned]]
+
+      if(length(valid_cols) > 0){
+
+        valid_cols[1]
+
+      }else{
+
+        always_assigned
+
+      }
+
+    })
+
+    tidy_avgs$measured_specie <- measured_species
+
+
+    # Fit model
+    dynfit <- tidy_avgs  %>%
+      group_by(grouping_features) %>%
+      summarise(fit = list(I(stats::optim(starting_values,
+                                          fn = dynamics_likelihood,
+                                          graph = graph,
+                                          formula_list = modeled_to_measured,
+                                          logit_fn = mean,
+                                          logit_fn_sd = sd,
+                                          coverage = coverage,
+                                          tls = as.numeric(tl),
+                                          sample_features = !!sample_feature,
+                                          feature_types = measured_specie,
+                                          lower = lower_bounds,
+                                          upper = upper_bounds,
+                                          method = "L-BFGS-B",
+                                          hessian = TRUE))))
+
+
+    # Get parameter estimates and uncertainties
+    for(n in 1:npars){
+
+      par_name <- parameter_names[n]
+      par_se_name <- paste0(par_name, "_se")
+
+      dynfit <- dynfit %>% dplyr::mutate(
+        !!par_name := purrr::map_dbl(fit, ~ .x$par[n]),
+        !!par_se_name := purrr::map_dbl(fit, ~ sqrt(solve(.x$hessian)[n])),
+      )
+    }
+
+
+  }else{
+
+    stop("Nothing to see here yet")
+
+  }
+
+
+  ### TO-DO Add a new table type to EZbakRData object and EZget()
+
+  return(dynfit)
+
+
+
+}
+
+
+
+# Function to evaluate formulas and generate a new named vector
+evaluate_formulas2 <- function(original_vector, formula) {
+  new_vector <- numeric()
+  response <- as.character(formula[[2]])
+  terms <- all.vars(formula[[3]])
+  expr <- formula[[3]]
+  env <- list2env(as.list(original_vector))
+  value <- eval(expr, envir = env)
+  new_vector[response] <- value
+  return(new_vector)
+}
+
+# Likelihood function for generalized dynamical systems modeling
+# with averages tables
+dynamics_likelihood <- function(parameter_ests, graph, formula_list = NULL,
+                                logit_fn, logit_fn_sd, coverage,
+                                tls, sample_features, feature_types,
+                                use_coverage = TRUE){
+  ### Step 1, construct A
+
+  # Parameters are on log-scale for ease of optimization
+  param_extend <- c(0, exp(parameter_ests))
+  param_graph <- matrix(param_extend[graph + 1],
+                        nrow = nrow(graph),
+                        ncol = ncol(graph),
+                        byrow = FALSE)
+
+  # A is same size as graph minus the "0" row and column
+  A <- matrix(0,
+              nrow = nrow(graph) - 1,
+              ncol = ncol(graph) - 1)
+
+  rownames(A) <- rownames(graph[-1,])
+  colnames(A) <- rownames(graph[-1,])
+
+
+
+  zero_index <- which(colnames(graph) == "0")
+
+  # Left as an exercise to the reader
+  # Just kidding, I'll document this somewhere.
+  # TO-DO: Should really compile documentation for all
+  # non-obvious mathematical results
+  diag(A) <- -rowSums(param_graph[-zero_index,])
+  A <- A + t(param_graph[-zero_index,-zero_index])
+
+
+  ### Step 2: infer general solution
+
+  Rss <- solve(a = A,
+               b = -param_graph[zero_index,-zero_index])
+
+
+  ev <- eigen(A)
+
+  lambda <- ev$values
+  V<- ev$vectors
+
+
+  ### Step 3: Infer data for actual measured species
+  all_ss <- c()
+  all_fns <- c()
+
+  for(n in seq_along(tls)){
+
+    tl <- tls[n]
+    sample_feature <- sample_features[n]
+    feature_type <- feature_types[n]
+
+    cs <- solve(V, -Rss)
+
+    exp_lambda <- exp(lambda*tl)
+
+    scaled_eigenvectors <- V %*% diag(exp_lambda*cs)
+
+    result_vector <- rowSums(scaled_eigenvectors) + Rss
+
+    names(result_vector) <- rownames(A)
+
+    # Evaluate the formulas
+    sample_formula <- formula_list[[sample_feature]]
+
+    sample_formula <- sample_formula[[which(sapply(1:length(sample_formula),
+                                                   function(x) all.vars(sample_formula[[x]])[1] == feature_type))]]
+
+    measured_levels <- evaluate_formulas2(result_vector, sample_formula)
+
+    names(Rss) <- rownames(A)
+    measured_ss <- evaluate_formulas2(Rss, sample_formula)
+
+    all_fns <- c(all_fns, measured_levels/measured_ss)
+    all_ss <- c(all_ss, measured_ss)
+
+  }
+
+  all_fns <- dplyr::case_when(
+    all_fns > 0.9999 ~ 0.9999,
+    all_fns < 0.0001 ~ 0.0001,
+    .default = all_fns
+  )
+
+
+  ### Step 4 calculate likelihood
+  ll <- dnorm(logit_fn,
+              EZbakR:::logit(all_fns),
+              logit_fn_sd,
+              log = TRUE)
+
+  if(use_coverage){
+
+    ll <- ll +
+      dnorm(coverage,
+            log10(all_ss),
+            1/((10^coverage)*(log(10)^2)),
+            log = TRUE)
+
+  }
+
+  if(!is.finite(sum(ll))){
+    browser()
+  }
+
+  return(-sum(ll))
 
 
 
