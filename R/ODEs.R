2539b3a9b4c0266053b25db502db4e065ba9cd61
isaacvock <isaac.vock@gmail.com>
2024-07-17 15:23:23 -0400

    Fix bugs in uncertainty quantification

--- R/ODEs.R
+++ R/ODEs.R
@@ -555,7 +555,7 @@ dynamics_likelihood <- function(parameter_ests, graph, formula_list = NULL,
       ll <- ll +
         stats::dnorm(coverage,
                      log10(all_ss),
-                     (1/((10^coverage)*(log(10)^2)))/sqrt(nreps),
+                     sqrt((1/((10^coverage)*(log(10)^2))))/sqrt(nreps),
                      log = TRUE)
 
     }
